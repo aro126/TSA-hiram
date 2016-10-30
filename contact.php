46cf50aca6db2175c2fc5c7533442a6c85331654
aro126 <ambrenott@outlook.com>
2016-10-29 21:32:47 -0400

    Delete contact.php

--- contact.php
+++
@@ -1,9 +0,0 @@
-<html>
-<body>
-
-<?php echo $_POST["FirstName"]; ?> <?php echo $_POST["LastName"]; ?> left a message.<br>
-Their email address is: <?php echo $_POST["Email"]; ?>
-Their message is: <?php echo $_POST["Message"]; ?.
-
-</body>
-</html>

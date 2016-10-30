e6d346d9b6e84eed5da2d3d04df596e1d7d8ea0b
aro126 <ambrenott@outlook.com>
2016-10-29 21:58:57 -0400

    Delete contact.php

--- contact.php
+++
@@ -1,9 +0,0 @@
-<html>
-<body>
-<?php echo $_POST["FirstName"]; ?> <?php echo $_POST["LastName"]; ?> 
-<br>
-<?php echo $_POST["Email"]; ?> 
-<br>
-<?php echp $_POST["Message"]; ?>
-</body>
-</html>

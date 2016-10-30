463778f1915844863c4372a8d7b45ba03cc28bd5
aro126 <ambrenott@outlook.com>
2016-10-29 21:51:47 -0400

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+<?php echo $_POST["FirstName"]; ?> <?php echo $_POST["LastName"]; ?> 
+<br>
+<?php echo $_POST["Email"]; ?> 
+<br>
+<?php echp $_POST["Message"]; ?>
+</body>
+</html>

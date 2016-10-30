44d92992eb3eb420fcb77f5a753472b2c8ecc6ed
aro126 <ambrenott@outlook.com>
2016-10-29 21:13:13 -0400

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,1 +1,9 @@
+<html>
+<body>
 
+<?php echo $_POST["FirstName"]; ?> <?php echo $_POST["LastName"]; ?> left a message.<br>
+Their email address is: <?php echo $_POST["Email"]; ?>
+Their message is: <?php echo $_POST["Message"]; ?.
+
+</body>
+</html>
